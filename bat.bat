16fa2dedc50c98b027802ef184e3d915cd0b4642
BeginMan <pythonsuper@gmail.com>
2014-03-29 09:01:47 +0800

    Auto push

---
+++ bat.bat
@@ -0,0 +1,6 @@
+cd Desktop/pytool
+git pull origin master
+git add .
+git commit -m "Auto push"
+git push origin master
+
--- filehandle.py
+++ filehandle.py
@@ -1,3 +1,26 @@
 #!/usr/bin/python
 #-*- coding:utf-8 -*-
 __author__ = 'beginman'
+import platform
+import os
+
+
+def ChoiceFile(filename):
+	"""选取文件分布在windows或Linux"""
+	filepath = '/home/beginman/github/blog/_post'
+	if (platform.system()).lower() == 'windows':
+		filepath = 'E:\\gitblog\\beginman.github.com\_posts'
+	files = os.listdir(filepath)
+	if os.path.isdir(filepath):
+		return files
+	#filename = raw_input('输入要处理的文件名或模糊名称：')
+	#if filename:
+
+def main():
+	print ChoiceFile('linux.md')
+
+
+if __name__ == '__main__':
+	main()
+
+
