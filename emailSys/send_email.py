ed316756e2098da8c83311aa04e42660f938aff9
BeginMan <pythonsuper@gmail.com>
2015-05-19 14:31:41 +0800

    remove pass

--- emailSys/send_email.py
+++ emailSys/send_email.py
@@ -1,6 +1,13 @@
 # coding=utf-8
-__author__ = 'fang'
+#!/usr/bin/env python
+# send_email.py - Send email
+# Copyright (C) 2015 BeginMan. <xinxinyu2011@163.com>
+
+__author__ = 'BeginMan'
 import smtplib
+import re
+import os
+import time
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
@@ -10,20 +17,60 @@ MAIL_USER = '1373763906'
 MAIL_PWD = '***********'
 MAIL_POSTFIX = 'qq.com' # 发件箱的后缀
 
-mailto_list = ['xinxinyu2011@163.com']
+
+err_send_mails = os.path.join(os.path.dirname(__file__), 'err_send_mails.txt')
+Template = os.path.join(os.path.dirname(__file__), 'template/')
+
+def log_email():
+    emails = os.path.join(os.path.dirname(__file__), 'emails.txt')
+    if not os.path.isfile(emails):
+        print u'没有找到邮箱文件'
+        return None
+
+    emails_list = []
+    with open(emails) as f:
+        for line in f.readlines():
+            try:
+                email = line.replace('\n', '')
+                if check_email(email):
+                    emails_list.append(email)
+                else:
+                    write_err_log(line)
+            except:
+                write_err_log(line)
+
+    return emails_list
+
+
+
+def write_err_log(err_email):
+    """记录错误邮箱以及没有发送成功的邮箱"""
+    with open(err_send_mails, 'a+') as err_file:
+        if err_email.endswith('\n'):
+            err_file.write(err_email)
+        else:
+            err_file.write(err_email+'\n')
+
+
+
+def check_email(email):
+    """检查邮件异常处理"""
+    if len(email) > 7:
+        if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", email) != None:
+            return 1
+    return 0
+
+
 
 
 class SendMail(object):
-    def __init__(self, host=MAIL_HOST, user=MAIL_USER, pwd=MAIL_PWD, postfix=MAIL_POSTFIX, mailto_list=mailto_list):
+    def __init__(self, host=MAIL_HOST, user=MAIL_USER, pwd=MAIL_PWD, postfix=MAIL_POSTFIX, mailto_list=None):
         self.host = host
         self.user = user
         self.pwd = pwd
         self.postfix = postfix
         self.mailto_list = mailto_list      # 收件人列表
 
-    def check_email(self):
-        """检查邮件异常处理"""
-        #TODO
 
 
     def gen_msg(self,msgObj,sub):
@@ -31,23 +78,30 @@ class SendMail(object):
         me="拨号精灵"+"<"+self.user+"@"+self.postfix+">"        # 头部显示
         msgObj['Subject'] = sub
         msgObj['From'] = me
-        msgObj['To'] = ";".join(self.mailto_list)
+        # msgObj['To'] = ";".join(self.mailto_list)             # 发送多个
+        msgObj['To'] = self.mailto_list                         # 发送单个
         return msgObj
 
 
     def send_mail_server(self, msgObj):
         """发送邮件服务"""
         try:
-            server = smtplib.SMTP()
-            server.connect(self.host)
-            server.login(self.user, self.pwd)
+            # server = smtplib.SMTP()
+            # server.connect(self.host)
+
+            # server = smtplib.SMTP_SSL(self.host, '465')
+            # server.login(self.user, self.pwd)
+
+            # 个人邮件服务器
+            server = smtplib.SMTP('123.56.102.154')
+
             server.sendmail(msgObj['From'], self.mailto_list, msgObj.as_string())
             server.close()
             return True
         except Exception, e:
+            write_err_log(self.mailto_list[0])
             print e
             return False
-
 
     def send_mail(self, sub, content, emial_type=1):
         """
@@ -82,17 +136,26 @@ class SendMail(object):
 
 
 
+def send_muti_email():
+    emails = log_email()
+    with open(os.path.join(Template, 'template1.html')) as html:
+        html = html.read()
+        for index, obj in enumerate(emails):
+            print u'正在向第%s位用户:%s 发送邮箱.....' % (index+1, obj)
+            if SendMail(mailto_list=obj).send_mail("拨号精灵致老朋友的一封信！", html, emial_type=2):
+                print u'发送成功！'
+            else:
+                print u'发送失败，已写入错误邮箱日志中!'
+
+            time.sleep(1)
+
+
 if __name__ == '__main__':
     # SendMail().send_mail("拨号精灵提示","测试项目！")
-    SendMail().send_mail("拨号精灵提示Html", "<h1>Hello</h1>", emial_type=2)
+    # SendMail().send_mail("拨号精灵提示Html", "<h1>Hello</h1>", emial_type=2)
     # SendMail().send_mail_with_attachment("拨号精灵提示File", {
     #     'c.py':'/Users/fang/tmp/c.py',
     # })
-
-    pass
-
-#Reference:http://www.cnblogs.com/xiaowuyi/archive/2012/03/17/2404015.html
-#454 Authentication failed, please open smtp flag first!： 原因是我使用的QQ邮箱没有开通POP3/SMTP服务，登录邮箱开启该服务即可
-#ImportError: No module named email.utils: 项目名称不要用Python标准库起名，如原先项目名:email.这样与标准库email冲突了
-
-#My blog:http://www.cnblogs.com/BeginMan/p/3443158.html
+    send_muti_email()
+    #454 Authentication failed, please open smtp flag first!： 原因是我使用的QQ邮箱没有开通POP3/SMTP服务，登录邮箱开启该服务即可
+    #ImportError: No module named email.utils: 项目名称不要用Python标准库起名，如原先项目名:email.这样与标准库email冲突了
--- spider/googleHost.py
+++ spider/googleHost.py
@@ -5,24 +5,39 @@
 import os
 import urllib
 import urllib2
+import cookielib
 import re
 import sys
 import datetime
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+# 获取一个保存cookie的对象
+cj = cookielib.LWPCookieJar()
+# 将一个保存cookie对象，和一个HTTP的cookie的处理器绑定
+cookie_support = urllib2.HTTPCookieProcessor(cj)
+# 创建一个opener，将保存了cookie的http处理器，还有设置一个handler用于处理http的URL的打开
+opener = urllib2.build_opener(cookie_support, urllib2.HTTPHandler)
+# 将包含了cookie、http处理器、http的handler的资源和urllib2对象板顶在一起
+urllib2.install_opener(opener)
+
 
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/57.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
 headers = {'User-Agent' : user_agent}
 dic = {
-        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept':'*/*',
         'Accept-Encoding':'gzip, deflate, sdch',
         'Accept-Language':'zh-CN,zh;q=0.8,en;q=0.6',
-        'Cache-Control':'max-age=0',
         'Connection':'keep-alive',
-        'Cookie':'hostspasscode=19066; _ga=GA1.2.1163994556.1420907346; _gat=1; Hm_lvt_e26a7cd6079c926259ded8f19369bf0b=1420903331; Hm_lpvt_e26a7cd6079c926259ded8f19369bf0b=1420907346',
+
+        # Cookie 变更
+        'Cookie':'hostspasscode=78053; Hm_lvt_e26a7cd6079c926259ded8f19369bf0b=1420903331,1422351431; Hm_lpvt_e26a7cd6079c926259ded8f19369bf0b=1422450312; _ga=GA1.2.1163994556.1420907346; _gat=1',
         'Host':'serve.netsh.org',
+        'Referer':'http://serve.netsh.org/pub/gethosts.php',
+        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.91 Safari/537.36',
+        'X-Requested-With':'XMLHttpRequest',
 }
+
 headers = dict(dic, **headers)
 
 def getGoogleHost():
@@ -30,14 +45,16 @@ def getGoogleHost():
     try:
         url = "http://serve.netsh.org/pub/hosts.php"
         data = urllib.urlencode({
-            "passcode":19066,
+            "passcode":88194,
             'gs':'on',
             'wk':'on',
             'twttr':'on',
             'fb':'on',
             'flkr':'on',
             'dpbx':'on',
-            'odrv':'on'
+            'yt':'off',
+            'validate':'b72bf',
+            'nolh':'off',
         })
         url = url+'?'+data
         req = urllib2.Request(url, headers=headers)
