e85a53e33e34389a9665db819d519179c3434de4
方朋 <xinxinyu2011@163.com>
2016-01-05 18:16:00 +0800

    字符串搜索与替换的方式

--- strings-and-text/replace_strings.py
+++ strings-and-text/replace_strings.py
@@ -30,3 +30,34 @@ def change_date(m):
 
 datepat = re.compile(r'(\d+)/(\d+)/(\d+)')
 print datepat.sub(change_date, text)        # Today is 27 Nov 2012. PyCon starts 13 Mar 2013.
+
+
+# 替换
+text = 'UPPER PYTHON, lower python, Mixed Python'
+re.findall('python', text, flags=re.IGNORECASE)
+# outputs: ['PYTHON', 'python', 'Python']
+re.sub('python', 'snake', text, flags=re.IGNORECASE)
+# outputs: 'UPPER snake, lower snake, Mixed snake'
+
+# 一个小缺陷，替换字符串并不会自动跟被匹配字符串的大小写保持一致。 为了修复这个，你可能需要一个辅助函数，就像下面的这样：
+
+
+def matchcase(word):
+    def replace(m):
+        text = m.group()
+        if text.isupper():
+            return word.upper()
+        elif text.islower():
+            return word.lower()
+        elif text[0].isupper():
+            return word.capitalize()
+        else:
+            return word
+    return replace
+
+
+print re.sub('python', matchcase('snake'), text, flags=re.IGNORECASE)
+# outputs: 'UPPER SNAKE, lower snake, Mixed Snake'
+
+# matchcase('snake') 返回了一个回调函数(参数必须是 match 对象)
+# sub() 函数除了接受替换字符串外，还能接受一个回调函数。
