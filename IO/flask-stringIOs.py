9eec326eb93fe0b64b19ac9ab3df25c3311f24ae
方朋 <xinxinyu2011@163.com>
2016-01-13 11:47:19 +0800

    StringIO应用

---
+++ IO/flask-stringIOs.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+"""
+关于StringIO参考
+    :copyright: (c) 2015 by fangpeng.
+    :license: MIT, see LICENSE for more details.
+"""
+__date__ = '1/13/16'
+
+import StringIO
+from flask import Flask, send_file
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Upload a StringIO object with send_file"""
+    strIO = StringIO.StringIO()
+    strIO.write("Hello, world\n")
+    strIO.write("中国")
+    strIO.seek(0)
+    return send_file(strIO,
+                     attachment_filename='strIO.txt',
+                     as_attachment=True)
+
+if __name__ == '__main__':
+    app.run()
+
+
---
+++ IO/io_operations_on_string.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+"""
+desc..
+    :copyright: (c) 2015 by fangpeng.
+    :license: MIT, see LICENSE for more details.
+"""
+__date__ = '1/13/16'
+
