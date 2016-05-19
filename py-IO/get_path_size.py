ef22f4059597bbe4d110d5a9695c378d55f414ed
beginman <xinxinyu2011@163.com>
2016-05-19 12:06:49 +0800

    get path size

---
+++ py-IO/get_path_size.py
@@ -0,0 +1,39 @@
+# coding=utf-8
+"""
+desc..
+    :copyright: (c) 2016 by fangpeng(@beginman.cn).
+    :license: MIT, see LICENSE for more details.
+"""
+import os
+
+def getFolderSize(folder):
+    total_size = os.path.getsize(folder)
+    for item in os.listdir(folder):
+        itempath = os.path.join(folder, item)
+        print itempath
+        if os.path.isfile(itempath):
+            print os.path.getsize(itempath)
+            total_size += os.path.getsize(itempath)
+        elif os.path.isdir(itempath):
+            total_size += getFolderSize(itempath)
+    return total_size
+
+
+def human(size):
+
+    B = "B"
+    KB = "KB"
+    MB = "MB"
+    GB = "GB"
+    TB = "TB"
+    UNITS = [B, KB, MB, GB, TB]
+    HUMANFMT = "%f %s"
+    HUMANRADIX = 1024.
+
+    for u in UNITS[:-1]:
+        if size < HUMANRADIX : return HUMANFMT % (size, u)
+        size /= HUMANRADIX
+
+    return HUMANFMT % (size,  UNITS[-1])
+
+print human(getFolderSize('/Users/fangpeng/Downloads/tools'))
