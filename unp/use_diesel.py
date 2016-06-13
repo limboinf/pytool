5bce09b83ee3807905587e05bbe926f8717a7a04
beginman <xinxinyu2011@163.com>
2016-06-13 11:59:46 +0800

    fixbug

---
+++ unp/use_diesel.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+"""
+https://github.com/dieseldev/diesel
+代码示例:https://github.com/dieseldev/diesel/tree/master/examples
+不建议使用diesel,这个库仅仅支持python2.x且两三年没更新了,连官网都找不到了
+
+    :copyright: (c) 2016 by fangpeng(@beginman.cn).
+    :license: MIT, see LICENSE for more details.
+"""
+import diesel
+
+class EchoServer(object):
+    def handler(self, remote_addr):
+        host, port = remote_addr[0], remote_addr[1]
+        print "Echo client connected from: %s:%d" % (host, port)
+        while 1:
+            try:
+                msg = diesel.until_eol()
+                y = ': '.join(["you said", msg])
+                diesel.send(y)
+            except Exception, e:
+                print e
+
+def main(server_port):
+    app = diesel.Application()
+    server = EchoServer()
+    app.add_service(diesel.Service(server.handler, server_port))
+    app.run()
+
+if __name__ == '__main__':
+    main(8899)
