ecbd20f6a82cbb64cf29e5310ca9d2fba61de24d
beginman <xinxinyu2011@163.com>
2016-06-13 14:50:48 +0800

    ok

--- test/mp.py
+++ test/mp.py
@@ -6,18 +6,18 @@ desc..
 """
 from math import radians, cos, sin, asin, sqrt
 
-cd*19
 
-#todo hihihi
+# todo hihihi
 
-def haversine(lon1, lat1, lon2, lat2): # 经度1，纬度1，经度2，纬度2 （十进制度数）
+
+def haversine(lon1, lat1, lon2, lat2):    # 经度1，纬度1，经度2，纬度2 （十进制度数）
     # 将十进制度数转化为弧度
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
     # haversine公式
     dlon = lon2 - lon1
     dlat = lat2 - lat1
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
     c = 2 * asin(sqrt(a))
     r = 6371        # 地球平均半径，单位为公里
     return c * r * 1000     # 返回米
