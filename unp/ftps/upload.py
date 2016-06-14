eb125c07d33b7ac8b71f095a72e88883c7759457
beginman <xinxinyu2011@163.com>
2016-06-14 16:54:34 +0800

    upload

---
+++ unp/ftps/upload.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+"""
+desc..
+    :copyright: (c) 2016 by fangpeng(@beginman.cn).
+    :license: MIT, see LICENSE for more details.
+"""
+import getpass
+
+password = getpass.getpass(prompt="{who} 请输入你的FTP密码:".format(who = getpass.getuser()))
+print password
+
